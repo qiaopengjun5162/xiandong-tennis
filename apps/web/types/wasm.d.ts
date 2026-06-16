3eeb48861717efcb7d4b76595f6b8fe649a1d199
qiaopengjun5162 <qiaopengjun0@gmail.com>
2026-06-16 09:07:00 +0800

    chore: remove unused wasm type declaration

--- apps/web/types/wasm.d.ts
+++
@@ -1,8 +0,0 @@
-import type { OptionValue, PersonalityInfo, Question } from '@xiandong/core';
-
-declare module '/pkg/xiandong_tennis_core.js' {
-  export default function init(): Promise<void>;
-  export function get_questions(): Question[];
-  export function calculate_result(answers_json: string): string;
-  export function get_personality_info(key: string): PersonalityInfo | undefined;
-}
